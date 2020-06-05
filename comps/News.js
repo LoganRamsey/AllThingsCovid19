8a846ee355c6d4340aa95b3de7182efedf46ac7d
Logan Ramsey <lr8fm@virginia.edu>
2020-06-05 18:54:40 -0400

    Update News.js

--- comps/News.js
+++ comps/News.js
@@ -1,7 +1,5 @@
-var apikey = '9a873ea2d67343ce80c357db11bcf569'
-
 const url =
-  "https://newsapi.org/v2/everything?q={coronavirus,COVID-19}&sortBy=popularity&language=en&pageSize=30&apiKey=" + apikey;
+  "https://newsapi.org/v2/everything?q={coronavirus,COVID-19}&sortBy=popularity&language=en&pageSize=30&apiKey=" + '9a873ea2d67343ce80c357db11bcf569';
 
 
   
